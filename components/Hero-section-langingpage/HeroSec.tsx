75e4a508ae2d3fd7fd093b5b401ffc6b1b551250
Asamu victor <asamuvictor03@gmail.com>
2023-01-18 12:54:09 +0100

    completed hero and started responsiveness

--- components/Hero-section-langingpage/HeroSec.tsx
+++ components/Hero-section-langingpage/HeroSec.tsx
@@ -3,6 +3,12 @@ import Link from 'next/link'
 import React from 'react'
 import apple from '../../public/images/svgexport-5.svg'
 import playstore from '../../public/images/svgexport-6.svg'
+import first from '../../public/images/svgexport-7.svg'
+import second from '../../public/images/svgexport-8.svg'
+import third from '../../public/images/svgexport-9.svg'
+import fourth from '../../public/images/svgexport-10.svg'
+import fifth from '../../public/images/svgexport-11.svg'
+import six from "../../public/images/svgexport-12.svg"
 const HeroSec = () => {
   return (
     <>
@@ -22,22 +28,52 @@ const HeroSec = () => {
        </div>
        <div className='w-1/2'>
         <div className='flex ml-60 mt-20'>
-           <div className='rounded-lg w-[150px] h-[150px] shadow-2xl mr-7 bg-[#FBF5F0] transition-all duration-300 hover:scale-90'>
-             hjhjhjhhhj
+           <div className='rounded-lg w-[150px] h-[170px] shadow-2xl mr-7 bg-[#FBF5F0] transition-all duration-300 hover:scale-90'>
+             <Image src={first} alt='' width={40} height={40} className='ml-3 mt-3'/>
+             <p className='font-apple font-medium ml-3 mt-3 text-[#8E6704]'>Airtime to Cash</p>
+             <div className='text-[13px] ml-3 mt-2 text-[rgb(82,82,84)]'>
+              Sell your airtime and get cash and or buy airtime.
+             </div>
            </div>
-           <div className='rounded-lg w-[150px] h-[150px] shadow-2xl mr-7 bg-[#F7F6FC] transition-all duration-300 hover:scale-90'>hello</div>
+           <div className='rounded-lg w-[150px] h-[170px] shadow-2xl mr-7 bg-[#F7F6FC] transition-all duration-300 hover:scale-90'>
+           <Image src={second} alt='' width={40} height={40} className='ml-3 mt-3'/>
+             <p className='font-apple font-medium ml-3 mt-3 text-[#4317C0]'>Cashpin</p>
+             <div className='text-[13px] ml-3 mt-2 text-[#525254]'>
+              Cashpin is a token that can be saved as cash.
+             </div>
+           </div>
         </div>
         <div className='flex ml-60 mt-6'>
-           <div className='rounded-lg w-[150px] h-[150px] shadow-2xl mr-7 bg-[#FBF1F1] transition-all duration-300 hover:scale-90'>
-             hjhjhjhhhj
+           <div className='rounded-lg w-[150px] h-[170px] shadow-2xl mr-7 bg-[#FBF1F1] transition-all duration-300 hover:scale-90'>
+           <Image src={third} alt='' width={40} height={40} className='ml-3 mt-3'/>
+             <p className='font-apple font-medium ml-3 mt-3 text-[#C02634]'>Send Funds</p>
+             <div className='text-[13px] ml-3 mt-2 text-[#525254]'>
+              Send money to an email address or any bank accounts.
+             </div>
            </div>
-           <div className='rounded-lg w-[150px] h-[150px] shadow-2xl mr-7 bg-[#F0F7F4] transition-all duration-300 hover:scale-90'>hello</div>
+           <div className='rounded-lg w-[150px] h-[170px] shadow-2xl mr-7 bg-[#F0F7F4] transition-all duration-300 hover:scale-90'>
+           <Image src={fourth} alt='' width={40} height={40} className='ml-3 mt-3'/>
+             <p className='font-apple font-medium ml-3 mt-3 text-[#1E8A5E]'>Send Funds</p>
+             <div className='text-[13px] ml-3 mt-2 text-[#525254]'>
+              Fund your vividpay wallet and send money to anyone.
+             </div>
+           </div>
         </div>
         <div className='flex ml-60 mt-6'>
-           <div className='rounded-lg w-[150px] h-[150px] shadow-2xl mr-7 bg-[#F1F4FB] transition-all duration-300 hover:scale-90'>
-             
+           <div className='rounded-lg w-[150px] h-[170px] shadow-2xl mr-7 bg-[#F1F4FB] transition-all duration-300 hover:scale-90'>
+           <Image src={fifth} alt='' width={40} height={40} className='ml-3 mt-3'/>
+             <p className='font-apple font-medium ml-3 mt-3 text-[#013AC0]'>Pay bills</p>
+             <div className='text-[13px] ml-3 mt-2 text-[#525254]'>
+              Pay electricity, TV subcription, data and more bills.
+             </div>
            </div>
-           <div className='rounded-lg w-[150px] h-[150px] shadow-2xl mr-7 bg-[#FEF5FF] transition-all duration-300 hover:scale-90'>hello</div>
+           <div className='rounded-lg w-[150px] h-[170px] shadow-2xl mr-7 bg-[#FEF5FF] transition-all duration-300 hover:scale-90'>
+           <Image src={six} alt='' width={40} height={40} className='ml-3 mt-3'/>
+             <p className='font-apple font-medium ml-3 mt-3 text-[#AE00BB]'>Get cards</p>
+             <div className='text-[13px] ml-3 mt-2 text-[#525254]'>
+              Get virtual Naira and dollar card in seconds.
+             </div>
+           </div>
         </div>
        </div>
       </div>
