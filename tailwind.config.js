7b93c5db86418f7be1ad5886ed914378589a603a
Asamu victor <asamuvictor03@gmail.com>
2023-01-19 21:29:58 +0100

    responsivesness for headers comp

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,27 @@ module.exports = {
     extend: {
       fontFamily:{
         'apple':['system-ui', '-apple-system', 'BlinkMacSystemFont', 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Open Sans', 'Helvetica Neue', 'sans-serif']
+      },
+      colors:{
+          papayawhip:{
+            light:"#fef4e4",
+            DEFAULT:"#ffefd5",
+            dark:"#fee5bc"
+          }
+      },
+      screens:{
+        'widescreen': {'raw':'(min-aspect-ratio:3/2)'},
+        'tallscreen': {'raw':'(max-aspect-ratio:13/20)'},
+      },
+      keyframes:{
+        'open-menu':{
+          '0%': { transform: 'scaleY(0)'},
+          '80%': { transform: 'scaleY(1.2)'},
+          '100%': { transform: 'scaleY(1)'},
+        },
+      },
+      animation: {
+        'open-menu': 'open-menu 0.5s ease-in-out forwards',
       }
     },
   },
