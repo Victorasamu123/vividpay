a82111a5574c3270b3dc1063b516434156bfa0ad
Avanin <asamuvictor03@gmail.com>
2023-04-17 22:23:18 +0100

    started home page

---
+++ pages/home/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'next/link';
+import Head from 'next/head';
+import Image from 'next/image';
+const index = () => {
+  return (
+    <>
+     
+    </>
+  )
+}
+
+export default index;
