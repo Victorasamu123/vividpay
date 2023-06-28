b67cc701e4a897bcba3b391a5f5f1a5593c78024
Avanin <asamuvictor03@gmail.com>
2023-06-28 01:55:16 +0100

    fund wallet page ui

--- pages/home/fundwallet.tsx
+++ pages/home/fundwallet.tsx
@@ -45,8 +45,16 @@ const Fundwallet = () => {
          {/* sidebar menu code */}
           <SideBar/>
        {/* middle account info code */}
-       <div id='two' className='lg:mt-[2%] lg:w-[53%] lg:h-[400px] lg:bg-white lg:ml-[2.3%] md:mt-[2%] md:w-[90%] md:h-[700px] md:bg-white md:ml-[5%] mt-[2%] w-[90%] ml-[5%] h-[500px] bg-white'>
-        
+       <div id='two' className='lg:mt-[2%] lg:w-[53%] lg:h-[600px] lg:ml-[2.3%] md:mt-[2%] md:w-[90%] md:h-[700px]  md:ml-[5%] mt-[2%] w-[90%] ml-[5%] h-[500px] rounded-xl flex justify-center flex-col items-center'>
+         <h3 className='text-[30px] font-medium mb-4'>Fund Wallet</h3>
+         <input type="email" className='w-[50%] h-[50px] bg-[#F3F3F3] rounded-[4px] focus:outline-[#623ECA] border-[#623ECA] border-[2px] pl-2 text-[#67656E] text-[14px] font-semibold mb-2' placeholder='Enter amount to deposit'/>
+         <input type="email" className='w-[50%] h-[50px] bg-[#F3F3F3] rounded-[4px] focus:outline-[#623ECA] border-[#623ECA] border-[2px] pl-2 text-[#67656E] text-[14px] font-semibold mb-2' value="12355555" readOnly/>
+         <input type="email" className='w-[50%] h-[50px] bg-[#F3F3F3] rounded-[4px] focus:outline-[#623ECA] border-[#623ECA] border-[2px] pl-2 text-[#67656E] text-[14px] font-semibold mb-4' value="Asamu victor" readOnly/>
+         <h3 className='text-[30px] font-medium mb-4'>Card Details</h3>
+         <input type="email" className='w-[50%] h-[50px] bg-[#F3F3F3] rounded-[4px] focus:outline-[#623ECA] border-[#623ECA] border-[2px] pl-2 text-[#67656E] text-[14px] font-semibold mb-2' placeholder='Enter card number'/>
+         <input type="email" className='w-[50%] h-[50px] bg-[#F3F3F3] rounded-[4px] focus:outline-[#623ECA] border-[#623ECA] border-[2px] pl-2 text-[#67656E] text-[14px] font-semibold mb-2' placeholder='Enter card expiry date'/>
+         <input type="email" className='w-[50%] h-[50px] bg-[#F3F3F3] rounded-[4px] focus:outline-[#623ECA] border-[#623ECA] border-[2px] pl-2 text-[#67656E] text-[14px] font-semibold' placeholder='Enter card CVV '/>
+         <button className='block mt-[15px] w-[50%] h-[55px] bg-[#623ECA] rounded-[4px] text-[#FFFFFF] text-[18px] font-semibold font-sans'>Proceed</button>
          </div>
          {/* {third section of dashboard} */}
         <ThirdBar/>
