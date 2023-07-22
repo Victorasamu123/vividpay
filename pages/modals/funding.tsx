c101e637552869b91b1f9dabe6b2900c039b1a64
Avanin <asamuvictor03@gmail.com>
2023-07-22 20:57:57 +0100

    modal for funding

--- pages/modals/funding.tsx
+++ pages/modals/funding.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import { MdCancel } from "react-icons/md";
 interface ModalProps {
     isOpen: boolean;
     onClose: () => void;
@@ -7,16 +7,26 @@ interface ModalProps {
   
 const Funding: React.FC<ModalProps> = ({ isOpen, onClose }) => {
     if (!isOpen) return null;
+
+    const iconStyle = {
+      width:'40px',
+      height:"40px",
+    }
   return (
     <>
     <div className="fixed inset-0 bg-gray-800 opacity-50"></div>
       <div className='fixed inset-0 flex items-center justify-center z-50'>
-        <div className='bg-white w-[50%] h-[400px] p-4 rounded shadow-lg flex justify-center items-center flex-col '>
+        <div className='bg-white lg:w-[50%] lg:h-[400px] lg:p-4 lg:rounded lg:shadow-lg md:w-[90%] md:h-[400px] md:p-4 md:rounded md:shadow-lg w-[90%] h-[400px] p-4 rounded shadow-lg'>
+          <div className='flex items-end justify-end' onClick={onClose}>
+            <MdCancel style={iconStyle}/>
+          </div>
+        <div className=' flex justify-center items-center flex-col '>
        <h3 className='text-[30px] font-medium mb-4'>Card Details</h3>
          <input type="email" className='lg:w-[70%] md:w-[70%] w-[75%] h-[50px] bg-[#F3F3F3] rounded-[4px] focus:outline-[#623ECA] border-[#623ECA] border-[2px] pl-2 text-[#67656E] text-[14px] font-semibold mb-2' placeholder='Enter card number'/>
          <input type="email" className='lg:w-[70%] md:w-[70%] w-[75%] h-[50px] bg-[#F3F3F3] rounded-[4px] focus:outline-[#623ECA] border-[#623ECA] border-[2px] pl-2 text-[#67656E] text-[14px] font-semibold mb-2' placeholder='Enter card expiry date'/>
          <input type="email" className='lg:w-[70%] md:w-[70%] w-[75%] h-[50px] bg-[#F3F3F3] rounded-[4px] focus:outline-[#623ECA] border-[#623ECA] border-[2px] pl-2 text-[#67656E] text-[14px] font-semibold' placeholder='Enter card CVV '/>
-         <button className='block mt-[15px] lg:w-[70%] md:w-[70%] w-[75%] h-[55px] bg-[#623ECA] rounded-[4px] text-[#FFFFFF] text-[18px] font-semibold font-sans' onClick={onClose}>Proceed</button>
+         <button className='block mt-[15px] lg:w-[70%] md:w-[70%] w-[75%] h-[55px] bg-[#623ECA] rounded-[4px] text-[#FFFFFF] text-[18px] font-semibold font-sans' >Proceed</button>
+         </div>
          </div>
      </div>
     </>
