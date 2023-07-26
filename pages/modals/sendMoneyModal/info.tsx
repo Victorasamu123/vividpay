7de2d96e5c9a15a4d030a7c1f1d85376fcf1b016
Avanin <asamuvictor03@gmail.com>
2023-07-26 21:22:12 +0100

    updated info modal

--- pages/modals/sendMoneyModal/info.tsx
+++ pages/modals/sendMoneyModal/info.tsx
@@ -18,7 +18,7 @@ const Info:React.FC<ModalProps> = ({isOpen,setisOpen}) => {
       <div className='fixed inset-0 flex items-center justify-center z-50'>
         <div className='bg-white lg:w-[50%] lg:h-[350px] lg:p-4 lg:rounded lg:shadow-lg md:w-[90%] md:h-[350px] md:p-4 md:rounded md:shadow-lg w-[90%] h-[350px] p-4 rounded shadow-lg'>
         <div className=' flex justify-center items-center flex-col '>
-       <h3 className='text-[30px] font-medium mb-4'>Transactions Successful</h3>
+       <h3 className='text-[30px] font-medium mb-4 text-center'>Transactions Successful</h3>
          <Image src={fail} alt=''/>
          <button className='block mt-[15px] lg:w-[70%] md:w-[70%] w-[75%] h-[55px] bg-[#623ECA] rounded-[4px] text-[#FFFFFF] text-[18px] font-semibold font-sans' onClick={okNah}>OK</button>
          </div>
