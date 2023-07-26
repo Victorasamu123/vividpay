b193c76838a2b80abc966f0d5fd9b7c3622392a1
Avanin <asamuvictor03@gmail.com>
2023-07-26 20:54:56 +0100

    data array comp

---
+++ components/amount.tsx
@@ -0,0 +1,24 @@
+const myAmountForData: {amount:string, type: string}[] = [
+  {amount:"Select Amount", type:"null"},
+  {amount:"40MB for ₦50" , type:" Daily"},
+  {amount:"100MB for ₦100" , type:" Daily"},
+  {amount:"200MB for ₦200" , type:" Daily"},
+  {amount:"350MB for ₦300" , type:" Daily"},
+  {amount:"1GB for ₦350" , type:" Daily"},
+  {amount:"2.5GB for ₦500" , type:" Daily"},
+  {amount:"1.5GB for ₦50" , type:" Daily"},
+  {amount:"3GB for ₦1000" , type:" Daily"},
+  {amount:"4.5GB for ₦1500" , type:" Daily"},
+  {amount:"6GB for ₦2000" , type:" Daily"},
+  {amount:"7.5GB for ₦2500" , type:" Daily"},
+  {amount:"10GB for ₦3000" , type:" Daily"},
+  {amount:"14GB for ₦4000" , type:" Daily"},
+  {amount:"20GB for ₦5000" , type:" Daily"},
+  {amount:"50GB for ₦10000" , type:" Daily"},
+  {amount:"120GB for ₦20000" , type:" Daily"},
+];
+
+
+export default myAmountForData;
+
+
--- pages/home/buydata.tsx
+++ pages/home/buydata.tsx
@@ -11,6 +11,7 @@ import mtn from "../../public/images/MTN-logo-459AAF9482-seeklogo.com.png"
 import airtel from "../../public/images/airtel.png"
 import glo from "../../public/images/1630540.png"
 import ninemobile from "../../public/images/download.jpg"
+import myAmountForData from '@/components/amount';
 const BuyData = () => {
   return (
     <>
@@ -37,7 +38,17 @@ const BuyData = () => {
        {/* middle account info code */}
        <div id='two' className='lg:mt-[2%] lg:w-[53%] lg:h-[600px] lg:ml-[2.3%] md:mt-[2%] md:w-[90%] md:h-[700px]  md:ml-[5%] mt-[2%] w-[90%] ml-[5%] h-[500px] rounded-xl flex justify-center flex-col items-center'>
        <div className='bg-white lg:w-[50%] md:w-[90%] w-[90%] h-[50px] mb-5 flex justify-center items-center rounded-xl shadow-sm'>
-            <h1 className='text-[26px] font-apple  font-medium'>Buy Airtime</h1>
+            <h1 className='text-[26px] font-apple  font-medium'>Buy Data</h1>
+        </div>
+        <div className='w-[100%] flex justify-center items-center flex-col'>
+        <div className='flex justify-between items-center lg:w-[50%] md:w-[90%] w-[90%] mb-5'>
+           <Image src={mtn} alt='' width={60}/>
+           <Image src={airtel} alt='' width={60}/>
+           <Image src={glo} alt='' width={60}/>
+           <Image src={ninemobile} alt='' width={60}/>
+        </div>
+        <input type="text" className='lg:w-[50%] md:w-[90%] w-[90%] h-[50px] bg-[#F3F3F3] rounded-[4px] focus:outline-[#623ECA] border-[#623ECA] border-[2px] pl-2 text-[#67656E] text-[14px] font-semibold mb-5' placeholder='Phone number should be at least 10 characters'/>
+         <select name="" id="" className='lg:w-[50%] md:w-[90%] w-[90%] h-[50px] bg-[#F3F3F3] rounded-[4px] focus:outline-[#623ECA] border-[#623ECA] border-[2px] pl-2 text-[#67656E] text-[14px] font-semibold mb-5'></select>
         </div>
        </div>
          {/* {third section of dashboard} */}
