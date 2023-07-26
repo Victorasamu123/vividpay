63395cca323bf5540e9e239fd4fb1a70b9a77233
Avanin <asamuvictor03@gmail.com>
2023-07-26 22:07:57 +0100

    started pay-bills page

---
+++ pages/home/paybills.tsx
@@ -0,0 +1,23 @@
+import React, { useState,useEffect } from 'react'
+import Head from 'next/head';
+import Image from 'next/image';
+import iconb from "../../public/images/svgexport-14.svg"
+import Headers from '@/components/header.tsx/headers';
+import people from "../../public/images/profile-pic.a9136072d073801df253.png"
+import axios from 'axios';
+import { useRouter } from 'next/router';
+import SideBar from '@/components/home/sidebar';
+import ThirdBar from '@/components/home/thirdbar';
+import Funding from '../modals/funding';
+import Enterpin from '../modals/enterpin';
+import Info from '../modals/info';
+
+const PayBills = () => {
+  return (
+    <>
+     handleClosePin
+    </>
+  )
+}
+
+export default PayBills;
