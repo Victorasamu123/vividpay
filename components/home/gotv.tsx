07c201013dbd9ceaadd635c74840a32c973ee497
Avanin <asamuvictor03@gmail.com>
2023-07-28 00:49:09 +0100

    gotv modal page

---
+++ components/home/gotv.tsx
@@ -0,0 +1,12 @@
+const GoTvPackage :{plan:string, amount:string, package:string}[]=[
+    {plan:"Select Package", amount:"", package:"Gotv"},
+    {plan:"GOtv Supa", amount:"₦6,400", package:"Gotv"},
+    {plan:"GOtv Max", amount:"₦4,850", package:"Gotv"},
+    {plan:"GOtv Joli", amount:"₦3,300", package:"Gotv"},
+    {plan:"GOtv Jinja", amount:"₦2,250", package:"Gotv"},
+    {plan:"GOtv Lite", amount:"₦1,100", package:"Gotv"},
+    {plan:"GOtv Lite(Quaterly)", amount:"₦2,900", package:"Gotv"},
+    {plan:"GOtv Lite(Annual)", amount:"₦8,600", package:"Gotv"},
+]
+
+export default GoTvPackage ;
