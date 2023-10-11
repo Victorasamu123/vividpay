d0b36b6bf8c365058ed32edd839d50ab4f1d6479
Avanin <asamuvictor03@gmail.com>
2023-10-11 22:33:51 +0100

    paybills page

--- pages/modals/info.tsx
+++ pages/modals/info.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image';
 import React from 'react'
-import success from "../../public/images/success.png"
+import success from "../../public/images/failoo-removebg-preview.png"
 import fail from "../../public/images/fail.png"
 interface ModalProps {
     isOpen:boolean;
@@ -21,7 +21,7 @@ const okNah=()=>{
         <div className='bg-white lg:w-[50%] lg:h-[350px] lg:p-4 lg:rounded lg:shadow-lg md:w-[90%] md:h-[350px] md:p-4 md:rounded md:shadow-lg w-[90%] h-[350px] p-4 rounded shadow-lg'>
         <div className=' flex justify-center items-center flex-col '>
        <h3 className='text-[30px] font-medium mb-4'>Transactions Successful</h3>
-         <Image src={success} alt=''/>
+         <Image src={success} alt='' width={400}/>
          <button className='block mt-[15px] lg:w-[70%] md:w-[70%] w-[75%] h-[55px] bg-[#623ECA] rounded-[4px] text-[#FFFFFF] text-[18px] font-semibold font-sans' onClick={okNah}>OK</button>
          </div>
          </div>
